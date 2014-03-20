df8e0426429d7a0401bab15d09ab44d5b555e994
Matija Skala <mskala@gmx.com>
2014-03-20 23:11:46 +0100

    non_std::module

---
+++ nonstd/module.cpp
@@ -0,0 +1,107 @@
+/*
+ * This file is part of MLib
+ * Copyright (C) 2014  Matija Skala <mskala@gmx.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "module"
+
+#ifdef WIN32
+#include <windows.h>
+#else
+#include <dlfcn.h>
+#endif
+
+using namespace non_std;
+
+static std::string suffix() {
+#ifdef WIN32
+    #ifdef __DEBUG__
+        return "d.dll";
+    #else
+        return ".dll";
+    #endif
+#else
+    #ifdef __APPLE__
+        #ifdef __DEBUG__
+                return "d.dylib";
+        #else
+                return ".dylib";
+        #endif
+    #else
+        #ifdef __DEBUG__
+                return "d.so";
+        #else
+                return ".so";
+        #endif
+    #endif
+#endif
+}
+
+bool non_std::module::open ( const std::string& __s )
+{
+    if ( is_open() )
+        close();
+#ifdef WIN32
+    if ( __s.empty() )
+        _M_handle = GetModuleHandle ( NULL );
+    else
+    {
+        std::string fileName;
+
+        for ( char c: __s )
+        {
+            if( !c )
+                break;
+            else if( c == '/' )
+                fileName += '\\';
+            else
+                fileName += c;
+        }
+
+        fileName += suffix();
+
+        _M_handle = LoadLibraryExA ( (LPSTR) fileName.c_str(), NULL, 
+                                 LOAD_WITH_ALTERED_SEARCH_PATH );
+    }
+#else
+    std::string fileName = __s + suffix();
+    _M_handle = dlopen ( fileName.c_str(), RTLD_NOW );
+#endif
+    return _M_handle;
+}
+
+bool non_std::module::close()
+{
+    if ( !is_open() )
+        return false;
+    __handle_type handle = _M_handle;
+    _M_handle = nullptr;
+#ifdef WIN32
+    return FreeLibrary ( handle );
+#else
+    return dlclose ( handle ) == 0;
+#endif
+}
+
+void* non_std::module::_M_symbol ( const char* __s )
+{
+#ifdef WIN32
+    return GetProcAddress ( _M_handle, __s );
+#else
+    return dlsym ( _M_handle, __s );
+#endif
+}
