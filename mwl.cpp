02307c7a6219df78edda95286dc1857093b3f117
Matija Skala <mskala@gmx.com>
2014-08-06 13:06:52 +0200

    wayland keyboard handling

--- mwl.cpp
+++ mwl.cpp
@@ -21,12 +21,15 @@
 #include <mglobal.h>
 #include <MEventHandler>
 #include <MDebug>
+#include <MKeys>
 #include <wayland-egl.h>
 #include <dlfcn.h>
 #include <unistd.h>
 #include <EGL/egl.h>
 #include <GL/gl.h>
 #include <linux/input.h>
+#include <sys/mman.h>
+#include <xkbcommon/xkbcommon.h>
 
 using namespace std;
 
@@ -47,10 +50,15 @@ class WaylandVideoInterface : public MVideoInterface
     struct wl_egl_window *wl_egl_window;
     struct wl_pointer *wl_pointer;
     struct wl_keyboard *wl_keyboard;
+    struct xkb_context *xkb_context;
+    struct xkb_keymap *xkb_keymap;
+    struct xkb_state *xkb_state;
     EGLDisplay egl_display;
     EGLSurface egl_surface;
     EGLConfig egl_config;
     EGLContext egl_context;
+    MKey keymap[0x100];
+    void keymap_init();
 };
 
 void WaylandVideoInterface::beginPaint()
@@ -85,14 +93,28 @@ bool WaylandVideoInterface::init()
         [] ( void *data, struct wl_pointer *wl_pointer, uint32_t time, uint32_t axis, wl_fixed_t value ) {},
     };
     static const struct wl_keyboard_listener wl_keyboard_listener = {
-        [] ( void *data, struct wl_keyboard *wl_keyboard, uint32_t format, int32_t fd, uint32_t size ) {},
+        [] ( void *data, struct wl_keyboard *wl_keyboard, uint32_t format, int32_t fd, uint32_t size ) {
+            auto _This = static_cast<WaylandVideoInterface*> ( data );
+            void* map_str = mmap ( nullptr, size, PROT_READ, MAP_SHARED, fd, 0 );
+            xkb_keymap_unref ( _This->xkb_keymap );
+            xkb_state_unref ( _This->xkb_state );
+            _This->xkb_keymap = xkb_keymap_new_from_string ( _This->xkb_context, static_cast<char*> ( map_str ), XKB_KEYMAP_FORMAT_TEXT_V1, static_cast<xkb_keymap_compile_flags> ( 0 ) );
+            _This->xkb_state = xkb_state_new ( _This->xkb_keymap );
+            munmap ( map_str, size );
+        },
         [] ( void *data, struct wl_keyboard *wl_keyboard, uint32_t serial, struct wl_surface *surface, struct wl_array *keys ) {},
         [] ( void *data, struct wl_keyboard *wl_keyboard, uint32_t serial, struct wl_surface *surface ) {},
         [] ( void *data, struct wl_keyboard *wl_keyboard, uint32_t serial, uint32_t time, uint32_t key, uint32_t state ) {
             auto _This = static_cast<WaylandVideoInterface*> ( data );
-            if ( key == KEY_ESC ) {
-                MEventHandler::handlers.top().quit();
-            }
+            const xkb_keysym_t* syms;
+            xkb_keysym_t sym = XKB_KEY_NoSymbol;
+            if ( xkb_state_key_get_syms ( _This->xkb_state, key + 8, &syms ) == 1 )
+                sym = syms[0];
+            MKey mKey = ( ( sym & 0xff00 ) == 0xff00 ) ? _This->keymap[sym & 0xff] : MKey ( sym & 0xff );
+            if ( state )
+                MEventHandler::handlers.top().key_pressed ( mKey, 0 );
+            else
+                MEventHandler::handlers.top().key_released ( mKey, 0 );
         },
         [] ( void *data, struct wl_keyboard *wl_keyboard, uint32_t serial, uint32_t mods_depressed, uint32_t mods_latched, uint32_t mods_locked, uint32_t group ) {},
     };
@@ -181,12 +203,15 @@ bool WaylandVideoInterface::init()
         return false;
     if ( !eglMakeCurrent ( egl_display, nullptr, nullptr, egl_context ) )
         return false;
+    xkb_context = xkb_context_new ( static_cast<xkb_context_flags> ( 0 ) );
+    keymap_init();
 
     return MVideoInterface::init();
 }
 
 void WaylandVideoInterface::fini()
 {
+    xkb_context_unref ( xkb_context );
     eglMakeCurrent ( egl_display, nullptr, nullptr, egl_context );
     eglDestroySurface ( egl_display, egl_surface );
     eglDestroyContext ( egl_display, egl_context );
@@ -224,6 +249,97 @@ bool WaylandVideoInterface::setVideoMode ( int x, int y )
     return true;
 }
 
-M_EXPORT WaylandVideoInterface interface;
+void WaylandVideoInterface::keymap_init()
+{
+    for ( MKey& key: keymap )
+        key = MKey::UNKNOWN;
+    keymap[XKB_KEY_BackSpace&0xFF] = MKey::BACKSPACE;
+    keymap[XKB_KEY_Tab&0xFF] = MKey::TAB;
+    keymap[XKB_KEY_Clear&0xFF] = MKey::CLEAR;
+    keymap[XKB_KEY_Return&0xFF] = MKey::RETURN;
+    keymap[XKB_KEY_Pause&0xFF] = MKey::PAUSE;
+    keymap[XKB_KEY_Escape&0xFF] = MKey::ESCAPE;
+    keymap[XKB_KEY_Delete&0xFF] = MKey::DELETE;
+
+    keymap[XKB_KEY_KP_0&0xFF] = MKey::KP0;           /* Keypad 0-9 */
+    keymap[XKB_KEY_KP_1&0xFF] = MKey::KP1;
+    keymap[XKB_KEY_KP_2&0xFF] = MKey::KP2;
+    keymap[XKB_KEY_KP_3&0xFF] = MKey::KP3;
+    keymap[XKB_KEY_KP_4&0xFF] = MKey::KP4;
+    keymap[XKB_KEY_KP_5&0xFF] = MKey::KP5;
+    keymap[XKB_KEY_KP_6&0xFF] = MKey::KP6;
+    keymap[XKB_KEY_KP_7&0xFF] = MKey::KP7;
+    keymap[XKB_KEY_KP_8&0xFF] = MKey::KP8;
+    keymap[XKB_KEY_KP_9&0xFF] = MKey::KP9;
+    keymap[XKB_KEY_KP_Insert&0xFF] = MKey::KP0;
+    keymap[XKB_KEY_KP_End&0xFF] = MKey::KP1; 
+    keymap[XKB_KEY_KP_Down&0xFF] = MKey::KP2;
+    keymap[XKB_KEY_KP_Page_Down&0xFF] = MKey::KP3;
+    keymap[XKB_KEY_KP_Left&0xFF] = MKey::KP4;
+    keymap[XKB_KEY_KP_Begin&0xFF] = MKey::KP5;
+    keymap[XKB_KEY_KP_Right&0xFF] = MKey::KP6;
+    keymap[XKB_KEY_KP_Home&0xFF] = MKey::KP7;
+    keymap[XKB_KEY_KP_Up&0xFF] = MKey::KP8;
+    keymap[XKB_KEY_KP_Page_Up&0xFF] = MKey::KP9;
+    keymap[XKB_KEY_KP_Delete&0xFF] = MKey::KP_PERIOD;
+    keymap[XKB_KEY_KP_Decimal&0xFF] = MKey::KP_PERIOD;
+    keymap[XKB_KEY_KP_Divide&0xFF] = MKey::KP_DIVIDE;
+    keymap[XKB_KEY_KP_Multiply&0xFF] = MKey::KP_MULTIPLY;
+    keymap[XKB_KEY_KP_Subtract&0xFF] = MKey::KP_MINUS;
+    keymap[XKB_KEY_KP_Add&0xFF] = MKey::KP_PLUS;
+    keymap[XKB_KEY_KP_Enter&0xFF] = MKey::KP_ENTER;
+    keymap[XKB_KEY_KP_Equal&0xFF] = MKey::KP_EQUALS;
+
+    keymap[XKB_KEY_Up&0xFF] = MKey::UP;
+    keymap[XKB_KEY_Down&0xFF] = MKey::DOWN;
+    keymap[XKB_KEY_Right&0xFF] = MKey::RIGHT;
+    keymap[XKB_KEY_Left&0xFF] = MKey::LEFT;
+    keymap[XKB_KEY_Insert&0xFF] = MKey::INSERT;
+    keymap[XKB_KEY_Home&0xFF] = MKey::HOME;
+    keymap[XKB_KEY_End&0xFF] = MKey::END;
+    keymap[XKB_KEY_Page_Up&0xFF] = MKey::PAGEUP;
+    keymap[XKB_KEY_Page_Down&0xFF] = MKey::PAGEDOWN;
+
+    keymap[XKB_KEY_F1&0xFF] = MKey::F1;
+    keymap[XKB_KEY_F2&0xFF] = MKey::F2;
+    keymap[XKB_KEY_F3&0xFF] = MKey::F3;
+    keymap[XKB_KEY_F4&0xFF] = MKey::F4;
+    keymap[XKB_KEY_F5&0xFF] = MKey::F5;
+    keymap[XKB_KEY_F6&0xFF] = MKey::F6;
+    keymap[XKB_KEY_F7&0xFF] = MKey::F7;
+    keymap[XKB_KEY_F8&0xFF] = MKey::F8;
+    keymap[XKB_KEY_F9&0xFF] = MKey::F9;
+    keymap[XKB_KEY_F10&0xFF] = MKey::F10;
+    keymap[XKB_KEY_F11&0xFF] = MKey::F11;
+    keymap[XKB_KEY_F12&0xFF] = MKey::F12;
+    keymap[XKB_KEY_F13&0xFF] = MKey::F13;
+    keymap[XKB_KEY_F14&0xFF] = MKey::F14;
+    keymap[XKB_KEY_F15&0xFF] = MKey::F15;
+
+    keymap[XKB_KEY_Num_Lock&0xFF] = MKey::NUMLOCK;
+    keymap[XKB_KEY_Caps_Lock&0xFF] = MKey::CAPSLOCK;
+    keymap[XKB_KEY_Scroll_Lock&0xFF] = MKey::SCROLLOCK;
+    keymap[XKB_KEY_Shift_R&0xFF] = MKey::RSHIFT;
+    keymap[XKB_KEY_Shift_L&0xFF] = MKey::LSHIFT;
+    keymap[XKB_KEY_Control_R&0xFF] = MKey::RCTRL;
+    keymap[XKB_KEY_Control_L&0xFF] = MKey::LCTRL;
+    keymap[XKB_KEY_Alt_R&0xFF] = MKey::RALT;
+    keymap[XKB_KEY_Alt_L&0xFF] = MKey::LALT;
+    keymap[XKB_KEY_Meta_R&0xFF] = MKey::RMETA;
+    keymap[XKB_KEY_Meta_L&0xFF] = MKey::LMETA;
+    keymap[XKB_KEY_Super_L&0xFF] = MKey::LSUPER; /* Left "Windows" */
+    keymap[XKB_KEY_Super_R&0xFF] = MKey::RSUPER; /* Right "Windows */
+    keymap[XKB_KEY_Mode_switch&0xFF] = MKey::MODE; /* "Alt Gr" key */
+    keymap[XKB_KEY_Multi_key&0xFF] = MKey::COMPOSE; /* Multi-key compose */
+
+    keymap[XKB_KEY_Help&0xFF] = MKey::HELP;
+    keymap[XKB_KEY_Print&0xFF] = MKey::PRINT;
+    keymap[XKB_KEY_Sys_Req&0xFF] = MKey::SYSREQ;
+    keymap[XKB_KEY_Break&0xFF] = MKey::BREAK;
+    keymap[XKB_KEY_Menu&0xFF] = MKey::MENU;
+    keymap[XKB_KEY_Hyper_R&0xFF] = MKey::MENU;   /* Windows "Menu" key */
+}
+
+WaylandVideoInterface interface;
 
 
