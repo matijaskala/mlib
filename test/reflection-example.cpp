68b3e2570fc44a5992769c11fd0a1ffb00a135bf
Matija Skala <mskala@gmx.com>
2014-10-10 17:04:50 +0200

    update reflection example

--- test/reflection-example.cpp
+++ test/reflection-example.cpp
@@ -1,20 +1,23 @@
 #include <mreflection.h>
 #include <iostream>
-#include <funcid>
 
-class Ukaz : public MReflection::Instance {
+class Ukaz {
 public:
-	struct Reflection : public MReflection {
-		Reflection();
-	};
-	MReflection& getClass();
-	void izpisi_stevilo ( int stevilo ) reflect(this,stevilo)
-	void izpisi_a() reflect(this)
+    struct Reflection : public MReflection {
+        Reflection();
+    };
+    MReflection& getClass();
+    void izpisi_stevilo ( int stevilo ) { std::cerr << stevilo << std::endl; }
+    void izpisi_stevilo () { std::cerr << "stevilo" << std::endl; }
+        template< typename... _Args >
+        void invoke ( const std::string& function_name, _Args... __args ) {
+            getClass().invoke(function_name, this, __args...);
+        }
 };
 
 Ukaz::Reflection::Reflection() : MReflection ( static_cast<Ukaz*>(0) ) {
-	funcs<void(void*,int)>() [method_name(Ukaz,izpisi_stevilo)] = [] ( void* _This, int stevilo ) { std::cerr << stevilo << std::endl; };
-	funcs<void()>() [method_name(Ukaz,izpisi_a)] = [] () { std::cerr << "a" << std::endl; };
+    M_REFLECT_METHOD(Ukaz,izpisi_stevilo,());
+    M_REFLECT_METHOD(Ukaz,izpisi_stevilo,(int));
 }
 
 Ukaz::Reflection Class;
@@ -23,14 +26,15 @@ MReflection& Ukaz::getClass() {
 	return Class;
 }
 
+#include <MDL>
 int main ( int argc, char** argv ) {
-	MReflection* reflection = MReflection::get ( "Ukaz" );
-	MReflection::Instance* instance = reflection->newInstance();
-	reflection->invoke ( "izpisi_stevilo(int)", instance, 2 );
-	reflection->invoke ( "izpisi_a()", instance );
-	instance->invoke ( "izpisi_stevilo(int)", 2 );
-	instance->invoke ( "izpisi_a()" );
-	dynamic_cast<Ukaz*> ( instance )->izpisi_stevilo ( 2 );
-	dynamic_cast<Ukaz*> ( instance )->izpisi_a();
-	return 0;
+    MReflection* reflection = MReflection::get ( "Ukaz" );
+    Ukaz* instance = reflection->newInstance<Ukaz>();
+    reflection->invoke ( "izpisi_stevilo(int)", instance, 2 );
+    reflection->invoke ( "izpisi_stevilo()", instance );
+    instance->invoke ( "izpisi_stevilo(int)", 2 );
+    instance->invoke ( "izpisi_stevilo()" );
+    instance->izpisi_stevilo(2);
+    instance->izpisi_stevilo();
+    return 0;
 }
