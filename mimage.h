f72f039a96bd3197df169b18b22a126c8fff1a43
Matija Skala <mskala@gmx.com>
2014-10-12 00:12:55 +0200

    Re-add MImage class

---
+++ mimage.h
@@ -0,0 +1,33 @@
+/*
+ * This file is part of MLib
+ * Copyright (C) 2014  Matija Skala <mskala@gmx.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef MIMAGE_H
+#define MIMAGE_H
+
+#include <MDataFile>
+#include <MSize>
+
+struct MImage : MDataFile
+{
+    MSize size;
+    bool alpha;
+    std::uint8_t* data;
+};
+
+#endif // MIMAGE_H
