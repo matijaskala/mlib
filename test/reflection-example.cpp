bf9e3b60bb666ed81661dae9f1cba04a9c844590
Matija Skala <mskala@gmx.com>
2014-10-13 15:19:22 +0200

    reflection-example: use access()

--- test/reflection-example.cpp
+++ test/reflection-example.cpp
@@ -3,24 +3,24 @@
 
 class Ukaz {
 public:
-    struct Reflection : public MReflection {
-        Reflection();
-    };
-    MReflection& getClass();
+    struct Reflection;
+    int stevilo;
+    static MReflection& getClass();
     void izpisi_stevilo ( int stevilo ) { std::cerr << stevilo << std::endl; }
-    void izpisi_stevilo () { std::cerr << "stevilo" << std::endl; }
+    void izpisi_stevilo () { std::cerr << stevilo << std::endl; }
         template< typename... _Args >
         void invoke ( const std::string& function_name, _Args... __args ) {
             getClass().invoke(function_name, this, __args...);
         }
 };
 
-Ukaz::Reflection::Reflection() : M_REFLECTION_CONSTRUCT(Ukaz) {
-    M_REFLECT_METHOD(Ukaz,izpisi_stevilo,());
-    M_REFLECT_METHOD(Ukaz,izpisi_stevilo,(int));
-}
-
-Ukaz::Reflection Class;
+static struct Ukaz::Reflection : public MReflection {
+    Reflection() : M_REFLECTION_CONSTRUCT(Ukaz) {
+        M_REFLECT_METHOD(Ukaz,izpisi_stevilo,());
+        M_REFLECT_METHOD(Ukaz,izpisi_stevilo,(int));
+        M_REFLECT_FIELD(Ukaz,stevilo);
+    }
+} Class;
 
 MReflection& Ukaz::getClass() {
 	return Class;
@@ -29,6 +29,7 @@ MReflection& Ukaz::getClass() {
 int main ( int argc, char** argv ) {
     MReflection* reflection = MReflection::get ( "Ukaz" );
     Ukaz* instance = reflection->newInstance<Ukaz>();
+    reflection->access<int> ( instance, "z" ) = 6;
     reflection->invoke ( "izpisi_stevilo(int)", instance, 2 );
     reflection->invoke ( "izpisi_stevilo()", instance );
     instance->invoke ( "izpisi_stevilo(int)", 2 );
