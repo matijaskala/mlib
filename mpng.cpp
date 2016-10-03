07d9ae729b89cdfa9ea4f965225f9d70eed041c3
Matija Skala <mskala@gmx.com>
2016-10-04 01:57:31 +0200

    improve png loading

--- mpng.cpp
+++ mpng.cpp
@@ -66,32 +66,40 @@ MResource* MPNG::load ( std::string file )
     std::ifstream stream { file, std::ios::binary };
     png_set_read_fn ( png, &stream, readData );
     png_read_info ( png, info );
-    png_uint_32 width = png_get_image_width ( png, info );
-    png_uint_32 height = png_get_image_height ( png, info );
-    png_byte bitDepth = png_get_bit_depth ( png, info );
-    png_byte colorType = png_get_color_type ( png, info );
+    png_uint_32 width, height;
+    int bitDepth, colorType, interlaceMethod;
+    png_get_IHDR ( png, info, &width, &height, &bitDepth, &colorType, &interlaceMethod, nullptr, nullptr );
     png_size_t stride = png_get_rowbytes ( png, info );
-    if ( bitDepth == 16 )
-        png_set_strip_16 ( png );
     switch ( colorType ) {
         case PNG_COLOR_TYPE_PALETTE:
             png_set_palette_to_rgb ( png );
             break;
         case PNG_COLOR_TYPE_GRAY:
-            if ( bitDepth < 8 )
-                png_set_expand_gray_1_2_4_to_8 ( png );
+            png_set_expand_gray_1_2_4_to_8 ( png );
+        case PNG_COLOR_TYPE_GRAY_ALPHA:
+            png_set_gray_to_rgb ( png );
             break;
     }
+    if ( png_get_valid ( png, info, PNG_INFO_tRNS ) )
+        png_set_tRNS_to_alpha ( png );
+    if ( bitDepth == 16 )
+        png_set_strip_16 ( png );
+    if ( bitDepth < 8 )
+        png_set_packing ( png );
+    if ( interlaceMethod != PNG_INTERLACE_NONE )
+        png_set_interlace_handling ( png );
+    png_set_filler ( png, 0xff, PNG_FILLER_AFTER );
     png_read_update_info ( png, info );
     bitDepth = png_get_bit_depth ( png, info );
     stride = png_get_rowbytes ( png, info );
     png_byte channels = png_get_channels ( png, info );
-    png_bytep data = static_cast<png_bytep>( malloc ( height * stride * sizeof *data ) );
+    png_bytep data = static_cast<png_bytep>( calloc ( height * stride, sizeof *data ) );
     png_bytepp rowPointers = new png_bytep[height];
     for ( png_uint_32 i = 0; i < height; i++ ) {
         rowPointers[i] = data + i * stride;
     }
     png_read_image ( png, rowPointers );
+    png_read_end ( png, info );
     delete[] rowPointers;
     png_destroy_read_struct ( &png, &info, nullptr );
     return new MImage{MSize ( width, height ), bitDepth * channels == 32, data};
