b2e5747750d44fb6ad782f14e4a9e136ea2333da
Matija Skala <mskala@gmx.com>
2025-11-09 19:20:34 +0100

    prettify mvariant.h

--- mvariant.h
+++ mvariant.h
@@ -1,6 +1,6 @@
 /*
  * This file is part of MLib
- * Copyright (C) 2014-2023  Matija Skala <mskala@gmx.com>
+ * Copyright (C) 2014-2025  Matija Skala <mskala@gmx.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -20,33 +20,33 @@
 #ifndef MVARIANT_H
 #define MVARIANT_H
 
-#include <variant>
-#include <sstream>
+#include <string>
 #include <twoway-vector.hh>
+#include <variant>
 
-class MVariant : std::variant<long double, std::string, twoway_vector<MVariant>>
+class MVariant
 {
     using LongDouble = long double;
     using String = std::string;
     using StringList = twoway_vector<String>;
     using List = twoway_vector<MVariant>;
-    using Base = std::variant<LongDouble, String, List>;
+    std::variant<LongDouble, String, List> base;
 
 public:
     MVariant ( std::nullptr_t = nullptr ) {}
-    MVariant ( LongDouble x ) : Base{x} {}
-    MVariant ( String x ) : Base{std::move(x)} {}
-    MVariant ( List x ) : Base{std::move(x)} {}
-    MVariant ( const char* s ) : MVariant(static_cast<String>(s)) {}
-    MVariant ( std::initializer_list<MVariant> l ) : MVariant(static_cast<List>(l)) {}
-    MVariant ( const MVariant& other ) : Base{static_cast<const Base&>(other)} {}
-    MVariant ( MVariant&& other ) : Base{static_cast<Base&&>(other)} {}
+    MVariant ( LongDouble x ) : base{x} {}
+    MVariant ( String x ) : base{std::move(x)} {}
+    MVariant ( List x ) : base{std::move(x)} {}
+    MVariant ( const char* s ) : base(static_cast<String>(s)) {}
+    MVariant ( std::initializer_list<MVariant> l ) : base(static_cast<List>(l)) {}
+    MVariant ( const MVariant& other ) : base(other.base) {}
+    MVariant ( MVariant&& other ) : base{std::move(other.base)} {}
     MVariant& operator= ( const MVariant& other ) {
-        Base::operator=(static_cast<const Base&>(other));
+        base = other.base;
         return *this;
     }
     MVariant& operator= ( MVariant&& other ) {
-        Base::operator=(static_cast<Base&&>(other));
+        base = std::move(other.base);
         return *this;
     }
     bool operator== ( const MVariant& other ) const {
@@ -57,13 +57,13 @@ public:
                 return self == other;
             else if constexpr (std::is_same_v<self_t, List> || std::is_same_v<other_t, List>)
                 return false;
-            else {
-                std::stringstream ss1, ss2;
-                ss1 << self;
-                ss2 << other;
-                return ss1.str() == ss2.str();
-            }
-        }, static_cast<const Base&>(*this), static_cast<const Base&>(other));
+            else if constexpr (std::is_same_v<self_t, String>)
+                return self == std::to_string(other);
+            else if constexpr (std::is_same_v<other_t, String>)
+                return std::to_string(self) == other;
+            else
+                static_assert(std::is_same_v<self_t, other_t>);
+        }, base, other.base);
     }
     bool operator!= ( const MVariant& other ) const {
         return !(*this == other);
@@ -83,36 +83,29 @@ public:
             using self_t = std::decay_t<decltype(self)>;
             if constexpr (std::is_same_v<self_t, long double>)
                 return self;
-            else if constexpr (std::is_same_v<self_t, String>) {
-                std::stringstream ss;
-                long double r;
-                ss << self;
-                ss >> r;
-                return r;
-            }
+            else if constexpr (std::is_same_v<self_t, String>)
+                return std::stold(self);
             else
                 return 0.0l;
-        }, static_cast<const Base&>(*this));
+        }, base);
     }
     String toString () const {
         return std::visit([] (auto&& self) {
             using self_t = std::decay_t<decltype(self)>;
             if constexpr (std::is_same_v<self_t, String>)
                 return self;
-            else if constexpr (std::is_same_v<self_t, long double>) {
-                std::stringstream ss;
-                String r;
-                ss << self;
-                ss >> r;
-                return r;
-            }
+            else if constexpr (std::is_same_v<self_t, long double>)
+                return std::to_string(self);
             else {
                 String r;
-                for (auto&& x: self)
+                for (auto&& x: self) {
+                    if (!r.empty())
+                        r += ", ";
                     r += x.toString();
+                }
                 return r;
             }
-        }, static_cast<const Base&>(*this));
+        }, base);
     }
     List toList () const {
         return std::visit([] (auto&& self) {
@@ -120,8 +113,8 @@ public:
             if constexpr (std::is_same_v<self_t, List>)
                 return self;
             else
-                return List{};
-        }, static_cast<const Base&>(*this));
+                return List{MVariant{self}};
+        }, base);
     }
 };
 
